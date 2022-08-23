8014fcb27441ed866983e891e5a1339f7aee877d
Tim Wijma <tim.wijma@gmail.com>
2022-08-23 23:52:34 +0200

    removed app.vue

--- src/App.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-import TestComponent from './components/TestComponent.vue';
-</script>
-
-<template>
-  <div class="container">
-    <TestComponent message="Hallo Wereld"></TestComponent>
-  </div>
-</template>
